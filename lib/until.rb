f1dc7ae2950cf472b2b7a5da1078a6b3a4f74144
Stephanie Messad <sabud1208@gmail.com>
2018-11-18 04:12:11 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,5 +1,5 @@
-def using_until
-        levitation_force = 6
-        until levitation_force <= 6
-
-end
+# def using_until
+#         levitation_force = 6
+#         until levitation_force <= 6
+#
+# end
